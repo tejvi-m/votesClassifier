2c6df34852a50829c4cabb944d15d07296b91557
Tejvi M <tejvimstays@gmail.com>
2019-11-25 18:14:13 +0530

    fixed cross validation for the model with bagging as well

--- client.cpp
+++ client.cpp
@@ -5,14 +5,14 @@
 int main(){
   vector<vector<char>> dataset = openFile("./data/votesData.txt");
 
-  pair<vector<double>, double> ret = crossValidate(10, dataset);
+  pair<vector<double>, double> ret = crossValidate(5, dataset);
 
   for(int i = 0; i < 10; i++){
     cout << ret.first[i] << endl;
   }
 
 
-  pair<vector<double>, double> retBagging = crossValidateWithBagging(10, dataset, 20, 15, 50);
+  pair<vector<double>, double> retBagging = crossValidateWithBagging(5, dataset, 15, 50);
 
   for(int i = 0; i < 10; i++){
     cout << retBagging.first[i] << endl;
