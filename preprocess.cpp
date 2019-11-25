fc289e207b1215ff38b8193436c7ad0113e6db1e
Tejvi M <tejvimstays@gmail.com>
2019-11-25 20:51:19 +0530

    removed unnecessary code

--- preprocess.cpp
+++ preprocess.cpp
@@ -6,25 +6,6 @@
 
 using namespace std;
 
-int computeHammingDistance(vector<char> point1, vector<char> point2){
-  //shoudl i include the target variabel?
-  //if comparing with a missign vlaue, dont coutn the missing value if the
-  //target values dont match
-
-  //not tested.
-  int size = point1.size();
-  int mismatches = 0;
-  for(int i = 0; i < size; i++){
-
-    //should only be run on imputed data? not sure how to handle this
-    //greater the distance, the more dissimilar they are
-    //also looking at the label for this?
-
-    if(!(point1[i] == point2[i] || point1[i] == '?' || point2[i] == '?')) mismatches++;
-  }
-  return mismatches;
-}
-
 vector<bool> getMax(const vector<vector<char>>& dataset){
 
   int numRows = dataset.size();
@@ -91,16 +72,6 @@ void shuffle(vector<vector<char>>& dataset){
   shuffle(dataset.begin(), dataset.end(), default_random_engine(seed));
 }
 
-void shuffleAndSplit(vector<vector<char>>& dataset, vector<vector<char>>& train, vector<vector<char>>& test, int split){
-
-  int size = dataset.size();
-  
-  shuffle(dataset);
-  copy(dataset.begin(), dataset.begin() + size - (dataset.size() * split) / 100, train.begin());
-  copy(dataset.begin() + size - (dataset.size() * split) / 100, dataset.end(), test.begin());
-
-}
-
 void saveDataset(string filename, vector<vector<char>>& dataset){
 
     ofstream outFile;
@@ -152,26 +123,12 @@ vector<vector<char>> openFile(string filename){
     }
 
     dataset.push_back(temp);
-    // count++;
   }
 
   fillMax(dataset);
-
-
-
-  //sanity check for hamming distance function
-  // cout << computeHammingDistance(dataset[1], dataset[2]);
-  // for(int i = 0; i < dataset[1].size(); i++){
-  //   cout << dataset[1][i] << " " << dataset[2][i] << endl;
-  // }
 
   file.close();
 
   saveDataset("./data/cleanedDataset.txt", dataset);
   return dataset;
 }
-//
-// int main(){
-//   openFile("./data/votesData.txt");
-//   return 0;
-// }
